4eee518b78003380881858fd8b12809a17854250
Kin Fei Lee <kilee@seekasia.com>
2024-09-19 00:09:29 +0800

    feat: use animation on theme change

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { IconButton } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { motion } from "framer-motion";
 
 interface ColorModeSwitcherProps {
   toggleTheme: () => void;
@@ -14,12 +15,19 @@ const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = ({
   const buttonProps = {
     size: "lg",
     isRound: true,
-    alignSelf: "flex-end",
+    variant: "ghost",
     icon: theme === "light" ? <MoonIcon /> : <SunIcon />,
     "aria-label": "Switch DarkMode",
   };
 
-  return <IconButton onClick={toggleTheme} {...buttonProps} />;
+  return (
+    <motion.div
+      whileTap={{ rotate: 180 }}
+      style={{ display: "flex", alignSelf: "flex-end" }}
+    >
+      <IconButton onClick={toggleTheme} {...buttonProps} />
+    </motion.div>
+  );
 };
 
 export default ColorModeSwitcher;
